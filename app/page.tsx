c01b563907237e36fb1b5bd8ddb4d5a8999580e9
chaming-yu <chamingyu1213@gmail.com>
2024-12-04 15:15:09 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-4">안녕하세요! 헬스 페이지입니다!</h1>
       <p className="text-lg mb-8">아래 버튼을 누르면 이용할 수 있습니다!</p>
       <button className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">
-        시작하기
+        시작하기!
       </button>
     </div>
   );
